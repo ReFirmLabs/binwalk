7c705dd972e815052a699d4b125395c2c2bf8fe3
devttys0 <heffnercj@gmail.com>
2014-02-02 20:49:21 -0500

    Updated with faster shannon implementation from bjornedstrom

--- src/binwalk/modules/entropy.py
+++ src/binwalk/modules/entropy.py
@@ -125,8 +125,14 @@ class Entropy(Module):
         entropy = 0
 
         if data:
+            length = len(data)
+            
+            seen = dict(((chr(x), 0) for x in range(0, 256)))
+            for byte in data:
+                seen[byte] += 1
+
             for x in range(0, 256):
-                p_x = float(data.count(chr(x))) / len(data)
+                p_x = float(seen[chr(x)]) / length
                 if p_x > 0:
                     entropy += - p_x*math.log(p_x, 2)
 
