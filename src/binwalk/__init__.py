581aa6b96444c6b2b6339c41267a598c2126450b
devttys0 <heffnercj@gmail.com>
2014-07-19 12:59:31 -0400

    Made modification of sys.path dependant on pyqtgraph import error

--- src/binwalk/__init__.py
+++ src/binwalk/__init__.py
@@ -3,7 +3,11 @@ __all__ = ['Modules', 'ModuleException']
 import sys
 import binwalk.core.common
 
-# This allows importing of the built-in pyqtgraph
-sys.path.append(binwalk.core.common.get_libs_path())
+# This allows importing of the built-in pyqtgraph if it
+# is not available on the system at run time.
+try:
+    import pyqtgraph
+except ImportError:
+    sys.path.append(binwalk.core.common.get_libs_path())
 
 from binwalk.core.module import Modules, ModuleException
