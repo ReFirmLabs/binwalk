a64c20f233d61a04d06f93b0a6a90a84a8a9605e
devttys0 <heffnercj@gmail.com>
2014-07-21 08:04:33 -0400

    Added configure option

--- src/binwalk/__init__.py
+++ src/binwalk/__init__.py
@@ -5,9 +5,6 @@ import binwalk.core.common
 
 # This allows importing of the built-in pyqtgraph if it
 # is not available on the system at run time.
-try:
-    import pyqtgraph
-except ImportError:
-    sys.path.append(binwalk.core.common.get_libs_path())
+sys.path.append(binwalk.core.common.get_libs_path())
 
 from binwalk.core.module import Modules, ModuleException
