ace65830a9e083e02a4ec9448c629a75a865e17f
devttys0 <heffnercj@gmail.com>
2014-08-02 20:40:18 -0400

    Added BSA signature, removed auto-generate src/binwalk/magic/binwalk from repo

--- setup.py
+++ setup.py
@@ -100,6 +100,11 @@ class CleanCommand(Command):
 
     def run(self):
         try:
+            os.remove("%s/magic/%s" % (MODULE_NAME, MODULE_NAME))
+        except Exception:
+            pass
+
+        try:
             remove_tree("build")
         except KeyboardInterrupt as e:
             raise e
