38ac807493a898b2d7ecc4a2b9e7a9857316f5ed
devttys0 <heffnercj@gmail.com>
2014-09-30 21:51:42 -0400

    Added convenience binwalk.scan function

--- src/binwalk/__init__.py
+++ src/binwalk/__init__.py
@@ -1,13 +1,17 @@
-__all__ = ['execute', 'Modules', 'ModuleException']
+__all__ = ['scan', 'execute', 'Modules', 'ModuleException']
 
 import sys
 import binwalk.core.common
 
 # This allows importing of the built-in pyqtgraph if it
 # is not available on the system at run time.
+# No longer needed, as pyqtgraph is no longer bundled with binwalk.
 sys.path.append(binwalk.core.common.get_libs_path())
 
 from binwalk.core.module import Modules, ModuleException
 
+# Convenience functions
+def scan(*args, **kwargs):
+    return Modules(*args, **kwargs).execute()
 def execute(*args, **kwargs):
     return Modules(*args, **kwargs).execute()
