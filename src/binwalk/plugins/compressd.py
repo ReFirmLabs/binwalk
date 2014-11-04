62e25031d4df3d70a07214a960a468f2da68d9aa
devttys0 <heffnercj@gmail.com>
2014-11-03 23:04:32 -0500

    Removed all C code from the project

--- src/binwalk/plugins/compressd.py
+++ src/binwalk/plugins/compressd.py
@@ -19,14 +19,20 @@ class CompressdPlugin(binwalk.core.plugin.Plugin):
     comp = None
 
     def init(self):
-        self.comp = binwalk.core.C.Library(self.COMPRESS42, self.COMPRESS42_FUNCTIONS)
+        #self.comp = binwalk.core.C.Library(self.COMPRESS42, self.COMPRESS42_FUNCTIONS)
+        # This plugin is currently disabled due to the need to move away from supporting C
+        # libraries and into a pure Python project, for cross-platform support and ease of
+        # installation / package maintenance. A Python implementation will likely need to
+        # be custom developed in the future, but for now, since this compression format is
+        # not very common, especially in firmware, simply disable it.
+        self.comp = None
 
     def scan(self, result):
-        if result.file and result.description.lower().startswith("compress'd data"):
+        if self.comp and result.file and result.description.lower().startswith("compress'd data"):
             fd = self.module.config.open_file(result.file.name, offset=result.offset, length=self.READ_SIZE)
             compressed_data = fd.read(self.READ_SIZE)
             fd.close()
-                        
+
             if not self.comp.is_compressed(compressed_data, len(compressed_data)):
                 result.valid = False
 
