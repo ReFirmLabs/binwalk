476766475c8e0ea0952d3da60f02069c6c53ad56
devttys0 <heffnercj@gmail.com>
2014-11-19 23:27:32 -0500

    Directory cleanup; signature updates

--- src/scripts/examples/signature_scan.py
+++ src/scripts/examples/signature_scan.py
@@ -8,6 +8,6 @@ try:
     for module in binwalk.scan(*sys.argv[1:], signature=True, quiet=True):
         print ("%s Results:" % module.name)
         for result in module.results:
-            print ("\t%s    0x%.8X    %s" % (result.file.name, result.offset, result.description))
+            print ("\t%s    0x%.8X    %s [%s]" % (result.file.name, result.offset, result.description, str(result.valid)))
 except binwalk.ModuleException as e:
     pass
