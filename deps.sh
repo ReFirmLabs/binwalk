65969ea50cc93482dfc5a7f5d1ee9cc8b69b9fcc
Dominic Chen <d.c.ddcc@gmail.com>
2016-02-27 02:10:58 -0500

    deps: check for package installation failure

--- deps.sh
+++ deps.sh
@@ -146,6 +146,11 @@ fi
 # Do the install(s)
 cd /tmp
 sudo $PKGCMD $PKGCMD_OPTS $PKG_CANDIDATES
+if [ $? -ne 0 ]
+    then
+    echo "Package installation failed: $PKG_CANDIDATES"
+    exit 1
+fi
 install_pip_package pyqtgraph
 install_pip_package capstone
 install_sasquatch
