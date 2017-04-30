09336bd28d7abf7e9572155a6f9c62566fc8e867
Martin Sundhaug <martinsundhaug@gmail.com>
2017-04-30 20:37:36 +0200

    tests: Dummy-test extraction

--- tests/test_srec.py
+++ tests/test_srec.py
@@ -13,7 +13,8 @@ def test_hello_world_simple_scan():
     scan_result = binwalk.scan(
         dirname(__file__) + '/input-vectors/hello-world.srec',
         signature=True,
-        quiet=True)
+        quiet=True,
+        extract=True)
     ok_(scan_result != [])
     eq_(len(scan_result), 1)
     eq_(scan_result[0].results[0].description,
