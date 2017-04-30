2d39ef92af6e5833d2277e730bdaa66da0af8b1a
Martin Sundhaug <martinsundhaug@gmail.com>
2017-04-30 19:31:03 +0200

    tests: Use flake8-formatting

--- tests/test_ihex.py
+++ tests/test_ihex.py
@@ -1,9 +1,20 @@
-import binwalk
-from nose.tools import ok_, eq_
 from os.path import dirname
 
+import binwalk
+from nose.tools import eq_, ok_
+
+
 def test_hello_world_simple_scan():
-    scan_result = binwalk.scan(dirname(__file__)+'/input-vectors/hello-world.ihex', signature=True,quiet=True)
+    '''
+    Test: Open hello-world ihex, scan for signatures
+    verify that only one signature is returned
+    verify that the only signature returned is Intel HEX data-signature
+    '''
+    scan_result = binwalk.scan(
+        dirname(__file__) + '/input-vectors/hello-world.ihex',
+        signature=True,
+        quiet=True)
     ok_(scan_result != [])
     eq_(len(scan_result), 1)
-    eq_(scan_result[0].results[0].description, 'Intel HEX data, record type: data')
+    eq_(scan_result[0].results[0].description,
+        'Intel HEX data, record type: data')
