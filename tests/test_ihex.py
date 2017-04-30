366b7f7282ca7fc0123614d9ae41f063461b049b
Martin Sundhaug <martinsundhaug@gmail.com>
2017-04-30 18:16:52 +0200

    tests: ihex: verify hit type

--- tests/test_ihex.py
+++ tests/test_ihex.py
@@ -6,3 +6,4 @@ def test_hello_world_simple_scan():
     scan_result = binwalk.scan(dirname(__file__)+'/input-vectors/hello-world.ihex', signature=True,quiet=True)
     ok_(scan_result != [])
     eq_(len(scan_result), 1)
+    eq_(scan_result[0].results[0].description, 'Intel HEX data, record type: data')
