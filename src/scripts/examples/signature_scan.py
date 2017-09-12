20dfcf86fd1544ebcbc5c93ff08805404e6b50db
Craig Heffner <heffnercj@gmail.com>
2017-09-12 14:03:50 -0400

    Cleaned up example scripts

--- src/scripts/examples/signature_scan.py
+++ src/scripts/examples/signature_scan.py
@@ -8,8 +8,11 @@ try:
     # and suppress the usual binwalk output.
     for module in binwalk.scan(*sys.argv[1:], signature=True, quiet=True):
         print ("%s Results:" % module.name)
+
         for result in module.results:
-            print ("\t%s    0x%.8X    %s [%s]" % (
-                result.file.name, result.offset, result.description, str(result.valid)))
+            print ("\t%s    0x%.8X    %s [%s]" % (result.file.name,
+                                                  result.offset,
+                                                  result.description,
+                                                  str(result.valid)))
 except binwalk.ModuleException as e:
     pass
