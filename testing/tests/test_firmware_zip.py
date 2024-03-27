101b608fead568ba515b6e45823ae00d828a62dc
Gianluigi Spagnuolo <gianluigi@exein.io>
2024-03-27 09:21:07 +0100

    fix: fix zip test

--- testing/tests/test_firmware_zip.py
+++ testing/tests/test_firmware_zip.py
@@ -10,8 +10,9 @@ def test_firmware_zip():
     '''
     expected_results = [
 	[0, 'Zip archive data, at least v1.0 to extract, name: dir655_revB_FW_203NA/'],
+    [51, 'Zip archive data, at least v2.0 to extract, compressed size: 6395868, uncompressed size: 6422554, name: dir655_revB_FW_203NA/DIR655B1_FW203NAB02.bin'],
+    [6395993, 'Zip archive data, at least v2.0 to extract, compressed size: 14243, uncompressed size: 61440, name: dir655_revB_FW_203NA/dir655_revB_release_notes_203NA.doc'],
 	[6410581, 'End of Zip archive, footer length: 22'],
-
     ]
 
     input_vector_file = os.path.join(os.path.dirname(__file__),
