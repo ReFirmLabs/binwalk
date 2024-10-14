b03e56135479985c1769516b1662348e9b763787
devttys0 <heffnercj@gmail.com>
2024-10-14 13:57:36 -0400

    Added env vars to force non-interactive install

--- dependencies/ubuntu.sh
+++ dependencies/ubuntu.sh
@@ -4,31 +4,32 @@
 SCRIPT_DIRECTORY=$(dirname -- "$( readlink -f -- "$0"; )")
 
 # Install dependencies from apt repository
-apt-get -y install p7zip-full \
-                        zstd \
-                        unzip \
-                        tar \
-                        sleuthkit \
-                        cabextract \
-                        curl \
-                        wget \
-                        git \
-                        lz4 \
-                        lzop \
-                        device-tree-compiler \
-                        unrar \
-                        unyaffs \
-                        python3-pip \
-                        build-essential \
-                        clang \
-                        liblzo2-dev \
-                        libucl-dev \
-                        liblz4-dev \
-                        libbz2-dev \
-                        zlib1g-dev \
-                        libfontconfig1-dev \
-                        liblzma-dev \
-                        libssl-dev
+DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get -y install \
+    p7zip-full \
+    zstd \
+    unzip \
+    tar \
+    sleuthkit \
+    cabextract \
+    curl \
+    wget \
+    git \
+    lz4 \
+    lzop \
+    device-tree-compiler \
+    unrar \
+    unyaffs \
+    python3-pip \
+    build-essential \
+    clang \
+    liblzo2-dev \
+    libucl-dev \
+    liblz4-dev \
+    libbz2-dev \
+    zlib1g-dev \
+    libfontconfig1-dev \
+    liblzma-dev \
+    libssl-dev
 
 # Install sasquatch Debian package
 curl -L -o sasquatch_1.0.deb "https://github.com/onekey-sec/sasquatch/releases/download/sasquatch-v4.5.1-4/sasquatch_1.0_$(dpkg --print-architecture).deb"
