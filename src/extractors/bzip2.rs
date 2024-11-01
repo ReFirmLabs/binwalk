40111215a8adc39f2018a832a91d3ba6e5b9c491
devttys0 <heffnercj@gmail.com>
2024-11-01 10:10:46 -0400

    Added extraction utility existence tests for extractors a-c

--- src/extractors/bzip2.rs
+++ src/extractors/bzip2.rs
@@ -2,6 +2,27 @@ use crate::extractors::common::{Chroot, ExtractionResult, Extractor, ExtractorTy
 use bzip2::{Decompress, Status};
 
 /// Defines the internal extractor function for decompressing BZIP2 files
+///
+/// ```
+/// use std::io::ErrorKind;
+/// use std::process::Command;
+/// use binwalk::extractors::common::ExtractorType;
+/// use binwalk::extractors::bzip2::bzip2_extractor;
+///
+/// match bzip2_extractor().utility {
+///     ExtractorType::None => panic!("Invalid extractor type of None"),
+///     ExtractorType::Internal(func) => println!("Internal extractor OK: {:?}", func),
+///     ExtractorType::External(cmd) => {
+///         if let Err(e) = Command::new(&cmd).output() {
+///             if e.kind() == ErrorKind::NotFound {
+///                 panic!("External extractor '{}' not found", cmd);
+///             } else {
+///                 panic!("Failed to execute external extractor '{}': {}", cmd, e);
+///             }
+///         }
+///     }
+/// }
+/// ```
 pub fn bzip2_extractor() -> Extractor {
     Extractor {
         utility: ExtractorType::Internal(bzip2_decompressor),
--- src/extractors/dtb.rs
+++ src/extractors/dtb.rs
@@ -1,6 +1,27 @@
 use crate::extractors;
 
 /// Describes how to run the dtc utility to extract DTB files
+///
+/// ```
+/// use std::io::ErrorKind;
+/// use std::process::Command;
+/// use binwalk::extractors::common::ExtractorType;
+/// use binwalk::extractors::dtb::dtb_extractor;
+///
+/// match dtb_extractor().utility {
+///     ExtractorType::None => panic!("Invalid extractor type of None"),
+///     ExtractorType::Internal(func) => println!("Internal extractor OK: {:?}", func),
+///     ExtractorType::External(cmd) => {
+///         if let Err(e) = Command::new(&cmd).output() {
+///             if e.kind() == ErrorKind::NotFound {
+///                 panic!("External extractor '{}' not found", cmd);
+///             } else {
+///                 panic!("Failed to execute external extractor '{}': {}", cmd, e);
+///             }
+///         }
+///     }
+/// }
+/// ```
 pub fn dtb_extractor() -> extractors::common::Extractor {
     extractors::common::Extractor {
         utility: extractors::common::ExtractorType::External("dtc".to_string()),
