a9e018953c757a565fa25fc36b63ecb4c637b78d
devttys0 <heffnercj@gmail.com>
2024-11-05 13:16:25 -0500

    Fixed openssl signature bug

--- src/structures/openssl.rs
+++ src/structures/openssl.rs
@@ -7,7 +7,7 @@ pub struct OpenSSLCryptHeader {
 
 /// Parse an OpenSSl crypto header
 pub fn parse_openssl_crypt_header(ssl_data: &[u8]) -> Result<OpenSSLCryptHeader, StructureError> {
-    let ssl_structure = vec![("magic", "u32"), ("salt", "u64")];
+    let ssl_structure = vec![("magic", "u64"), ("salt", "u64")];
 
     if let Ok(ssl_header) = common::parse(ssl_data, &ssl_structure, "big") {
         return Ok(OpenSSLCryptHeader {
