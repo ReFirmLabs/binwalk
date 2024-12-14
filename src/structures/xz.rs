23b3aa54a35a8306787ffae0d6889b5619d0cc9a
devttys0 <heffnercj@gmail.com>
2024-12-14 15:16:34 -0500

    Improved XZ signaturing, added support for malformed XZ streams

--- src/structures/xz.rs
+++ src/structures/xz.rs
@@ -24,33 +24,3 @@ pub fn parse_xz_header(xz_data: &[u8]) -> Result<usize, StructureError> {
 
     Err(StructureError)
 }
-
-/// Parse and validate an XZ footer, returns the footer size
-pub fn parse_xz_footer(xz_data: &[u8]) -> Result<usize, StructureError> {
-    const FOOTER_SIZE: usize = 12;
-    const CRC_DATA_SIZE: usize = 6;
-    const CRC_START_INDEX: usize = 4;
-
-    let xz_footer_structure = vec![
-        ("footer_crc", "u32"),
-        ("backward_size", "u32"),
-        ("flags", "u16"),
-        ("magic", "u16"),
-    ];
-
-    // Parse the stream footer
-    if let Ok(xz_footer) = common::parse(xz_data, &xz_footer_structure, "little") {
-        // Calculate the start and end offsets of the CRC'd data
-        let crc_start = CRC_START_INDEX;
-        let crc_end = crc_start + CRC_DATA_SIZE;
-
-        // Validate the stream footer
-        if let Some(crc_data) = xz_data.get(crc_start..crc_end) {
-            if crc32(crc_data) == (xz_footer["footer_crc"] as u32) {
-                return Ok(FOOTER_SIZE);
-            }
-        }
-    }
-
-    Err(StructureError)
-}
