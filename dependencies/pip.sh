00d3ecfe3188db4246aa2bc4fc70e87ddde1033d
devttys0 <heffnercj@gmail.com>
2025-04-05 18:11:58 -0400

    Fixed pip deps install errors

--- dependencies/pip.sh
+++ dependencies/pip.sh
@@ -2,9 +2,11 @@
 # Install pip dependencies.
 # Requires that pip3 is already installed.
 
-pip3 install uefi_firmware
-pip3 install jefferson
-pip3 install ubi-reader
-pip3 install --upgrade lz4 zstandard git+https://github.com/clubby789/python-lzo@b4e39df
-pip3 install --upgrade git+https://github.com/marin-m/vmlinux-to-elf
+PIP_OPTIONS='--break-system-packages'
+
+pip3 install uefi_firmware $PIP_OPTIONS
+pip3 install jefferson $PIP_OPTIONS
+pip3 install ubi-reader $PIP_OPTIONS
+pip3 install --upgrade lz4 zstandard git+https://github.com/clubby789/python-lzo@b4e39df $PIP_OPTIONS
+pip3 install --upgrade git+https://github.com/marin-m/vmlinux-to-elf $PIP_OPTIONS
 
